61f3c6ae93d83ca0f79a605466d484ecc4c6d9ff
Sleepwalker <sleepwalkerpasha@ya.ru>
2023-02-04 04:10:29 +0300

    dev: make test FilmFragment

--- app/src/main/java/ru/tinkoff/edu/popularfilms/ui/MainActivity.kt
+++ app/src/main/java/ru/tinkoff/edu/popularfilms/ui/MainActivity.kt
@@ -1,13 +1,25 @@
 package ru.tinkoff.edu.popularfilms.ui
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import ru.tinkoff.edu.popularfilms.R
+import androidx.appcompat.app.AppCompatActivity
+import ru.tinkoff.edu.popularfilms.databinding.ActivityMainBinding
+import ru.tinkoff.edu.popularfilms.ui.fragments.FilmFragment
 
 class MainActivity : AppCompatActivity() {
+
+    private val binding: ActivityMainBinding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+        loadData()
+    }
 
+    private fun loadData() {
+        supportFragmentManager.beginTransaction()
+            .add(binding.mainScreenContainer.id, FilmFragment.newInstance())
+            .commit()
     }
 }
--- app/src/main/java/ru/tinkoff/edu/popularfilms/ui/fragments/FilmFragment.kt
+++ app/src/main/java/ru/tinkoff/edu/popularfilms/ui/fragments/FilmFragment.kt
@@ -1,12 +1,25 @@
 package ru.tinkoff.edu.popularfilms.ui.fragments
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import ru.tinkoff.edu.popularfilms.R
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import ru.tinkoff.edu.popularfilms.databinding.FragmentFilmBinding
 
-class FilmFragment : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_film_fragment)
+class FilmFragment : Fragment() {
+
+    private lateinit var binding: FragmentFilmBinding
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+        binding = FragmentFilmBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    companion object {
+
+        fun newInstance(): FilmFragment {
+            return FilmFragment()
+        }
     }
 }
